a6b839d9429f9a5464f8d53b72b3217ac7e300c6
Albin Joseph <albinjoseph@rapidvaluesolutions.com>
2015-03-19 14:27:32 +0530

    modified:   javaMainClass.class 	modified:   javaMainClass.java

--- javaMainClass.java
+++ javaMainClass.java
@@ -6,6 +6,20 @@ public  class javaMainClass {
         System.out.println("My First Java Program in my system");
         
         System.out.println("Hi how are you?----");
+        
+        javaMainClass obj=new javaMainClass();
+        int result=obj.addition(10,15);
+        System.out.println("Result:"+result);
+    }
+    
+    
+    /*-------------------------------------------------------------
+     sample method for argument passing and value returm
+     -------------------------------------------------------------*/
+    public int addition(int one,int two)
+    {
+        int result=one+two;
+        return result;
     }
 }
 
