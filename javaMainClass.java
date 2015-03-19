4e0c7b2e4825bec776850e3210c36a9a01368a99
Albin Joseph <albinjoseph@rapidvaluesolutions.com>
2015-03-19 13:47:32 +0530

    add javaMainClass.java

---
+++ javaMainClass.java
@@ -0,0 +1,9 @@
+public  class javaMainClass {
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, World");
+        System.out.println("My First Java Program in my system");
+    }
+}
+
