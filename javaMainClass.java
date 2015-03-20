cc183c3c8fc0867e3d159de8c517d628dbee0789
Albin Joseph <albinjoseph@rapidvaluesolutions.com>
2015-03-20 09:55:59 +0530

    new file:   Employee.class 	new file:   Employee.java 	modified:   javaMainClass.class 	modified:   javaMainClass.java

--- javaMainClass.java
+++ javaMainClass.java
@@ -32,6 +32,25 @@ public  class javaMainClass {
         System.out.println("------------Benz object-----------");
         Benz benz=new Benz();
         benz.vehicleMethod();
+        
+        
+        //Employee class object
+        /* Create two objects using constructor */
+        System.out.println("------------Employee object-----------");
+        Employee empOne = new Employee("James Smith");
+        Employee empTwo = new Employee("Mary Anne");
+        
+        // Invoking methods for each object created
+        empOne.empAge(26);
+        empOne.empDesignation("Senior Software Engineer");
+        empOne.empSalary(1000);
+        empOne.printEmployee();
+        
+        empTwo.empAge(21);
+        empTwo.empDesignation("Software Engineer");
+        empTwo.empSalary(500);
+        empTwo.printEmployee();
+        
     }
     
     
